c63261df8973e2aef8e700bfcbb0fad39f13b32d
BterceroIII <b1tercero.cruz@gmail.com>
2025-01-24 01:10:13 -0600

    ✨ Agregar funcionalidad de administración de productos: implementar la página de productos con paginación, tabla de productos y validación de parámetros de búsqueda.

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,47 @@
-export default function ProductsPage() {
-  return <div>Products Page</div>;
+import ProductsTable from "@/components/products/ProductsTable";
+import Heading from "@/components/UI/Heading";
+import Pagination from "@/components/UI/Pagination";
+import { ProductsResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import { redirect } from "next/navigation";
+
+
+async function getProducts(take: number, skip: number) {
+  const url = `${process.env.API_URL}/product?take=${take}&skip=${skip}`;
+  const req = await fetch(url);
+  const json = await req.json();
+  const data = ProductsResponseSchema.parse(json);
+
+  return {
+    products: data.products,
+    total: data.total
+  };
+}
+
+type SearchParams = Promise<{page: string,}>
+
+export default async function ProductsPage({searchParams}: {searchParams: SearchParams}) {
+  
+  const {page} = await searchParams;
+  if(!isValidPage(+page)) redirect('/admin/products?page=1');
+
+  const productsPerPage = 10;
+  const skip = (+page -1) * productsPerPage;
+  const {products, total} = await getProducts(productsPerPage, skip);
+  const totalPages = Math.ceil(total / productsPerPage);
+  if(+page > totalPages) redirect('/admin/products?page=1');
+
+  return(
+    <>
+      <Heading>Administrar Productos</Heading>
+
+      <ProductsTable products={products} />
+
+      <Pagination
+          page={+page}
+          totalPages={totalPages}
+          baseUrl="/admin/products"
+      />
+    </>
+  )
 }
---
+++ components/UI/Pagination.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+type PaginationProps = {
+    page: number;
+    totalPages: number;
+    baseUrl: string;
+}
+
+export default function Pagination({
+  page,
+  totalPages,
+  baseUrl
+}: PaginationProps) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+  return (
+    <nav className="flex justify-center py-10">
+      {page > 1 && (
+        <Link
+          href={`${baseUrl}?page=${page - 1}`}
+          className="px-4 py-2 mx-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors"
+        >
+          &laquo;
+        </Link>
+      )}
+
+      {pages.map((currentPage) => (
+        <Link
+          key={currentPage}
+          href={`${baseUrl}?page=${currentPage}`}
+          className={`px-4 py-2 mx-2 rounded-lg ${currentPage === page ? "bg-gray-300" : "bg-gray-100"} hover:bg-gray-200 transition-colors `}
+        >
+          {currentPage}
+        </Link>
+      ))}
+
+        {page < totalPages && (
+            <Link
+                href={`${baseUrl}?page=${page + 1}`}
+                className="px-4 py-2 mx-2 rounded-lg bg-gray-100 hover:bg-gray-200 transition-colors"
+            >
+                &raquo;
+            </Link>
+        )}
+    </nav>
+  );
+}
